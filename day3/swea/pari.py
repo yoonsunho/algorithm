bec0a92c3ab88bfb2381a13ffe2770df4cf96fa0
yoonsunho <y_sunho@naver.com>
2025-02-09 13:15:31 +0900

    pycharm

--- day3/swea/pari.py
+++ day3/swea/pari.py
@@ -1,20 +1,25 @@
-# import sys
-# sys.stdin = open("pari.txt", "r")
+import sys
+sys.stdin = open("pari.txt", "r")
 
 T = int(input())
 
 for test_case in range(1, T + 1):
 
-    N, M = map(int,input().split())     # 5 <= N <=15
+    N, M = map(int, input().split())     # 5 <= N <=15
 
-    area = [list(map(int,input().split())) for _ in range(N) ]
+    area = [list(map(int, input().split())) for _ in range(N)]
 
     max_v = 0
     for i in range(N-M+1):
-       pari_sum = 0
        for j in range(N-M+1):
-            pari_sum += area[i][j]
-            max_v = max(pari_sum,max_v)
+           pari_sum = 0
+           for l in range(M+1):
+               for k in range(M+1):
+                   pari_sum += area[i+l][j+k]
+            max_v = max(max_v, pari_sum)
+
+            # pari_sum += area[i][j]
+            # max_v = max(pari_sum, max_v)
 
     
     print(f'#{test_case} {max_v}')
