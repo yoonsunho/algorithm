b9684a0ce71d4ff01e5e373a148579ee8968d1e7
sunho <y_sunho@naver.com>
2025-02-11 17:58:22 +0900

    d

--- day5/baekoon.py
+++ day5/baekoon.py
@@ -1,3 +1,25 @@
+# 내 풀이
+arr = list(input())
+
+N = len(arr)    # 1<=N<=1000
+
+for i in range(N):
+    if arr[i] == 'Y':
+        arr[i] = 1
+    else:
+        arr[i] = 0
+count = 0
+
+for i in range(1, N+1):
+    if arr[i-1] == 1:
+        count += 1
+        arr[i-1] -= 1
+        for j in range(i+1, N+1):
+            if j % i == 0:
+                arr[j-1] = 1 - arr[j-1]
+print(count)
+
+# 강사님 풀이
 arr = list(input())
 
 N = len(arr)    # 1<=N<=1000
@@ -17,4 +39,11 @@ for i in range(1, N+1):
             if j % i == 0:
                 arr[j-1] = 1 - arr[j-1]
 
-print(count)
+# 이 부분 추가 # 모든 전구를 끌 수 없다면 -1 출력
+for a in arr:
+    if a == 0:
+        print("-1")
+        break
+else:
+    print(count)
+
---
+++ day5/swea/10804.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open("txt/10804.txt", "r")
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+
+    str_ori = list(input())
+    str_ori.reverse()
+
+    for i in range(len(str_ori)):
+        if str_ori[i] == 'p':
+            str_ori[i] = 'q'
+        elif str_ori[i] == 'q':
+            str_ori[i] = 'p'
+        if str_ori[i] == 'b':
+            str_ori[i] = 'd'
+        if str_ori[i] == 'd':
+            str_ori[i] = 'b'
+
+    result = ''.join(str_ori)
+
+    print(f'#{test_case} {result}')
