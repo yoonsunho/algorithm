5d8b3ce822471ec668b530dcb59774fc4dad97cc
yoonsunho <y_sunho@naver.com>
2025-02-20 20:50:21 +0900

    12

--- day9/swea/21675.py
+++ day9/swea/21675.py
@@ -1,45 +1,51 @@
 import sys
 sys.stdin = open("txt/4874.txt", "r")
 
+
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
 
     fx = input().split()
-    print(fx)
+    # print(fx)
     stack = [0] * 256
     top = -1
 
     for x in fx:
-        if x not in '+-*/.':     # 피연산자라면
+        if x not in '+-*/.':     # 피연산자라면(숫자)
             # count_num
             top += 1
             stack[top] = int(x)
-        elif x not in ".":
-            if stack[0] != 0 and stack[1] != 0:
-                op2 = stack[top]
+        elif x != ".":
+            if top > 0:
+                op2 = int(stack[top])
                 stack[top] = 0
                 top -= 1
-                op1 = stack[top]
+                op1 = int(stack[top])
                 stack[top] = 0
                 top -= 1
                 if x == "+":
-                    a = op1 + op2
+                    a = int(op1 + op2)
                 elif x == "*":
-                    a = op1 * op2
+                    a = int(op1 * op2)
                 elif x == "/":
-                    a = op1 / op2
+                    a = int(op1 // op2)
                 elif x == "-":
-                    a = op1 - op2
+                    a = int(op1 - op2)
+                elif x == "%":
+                    a = int(op1 % op2)
                 top += 1
                 stack[top] = a
 
             else:
                 ans = 'error'
-        else:        # 연산자라면
+                break
+        else:        # . 라면
+            if top == 0:
+                ans = stack[top]
+            else:
+                ans = 'error'
 
-            ans = stack[top]
-            break
 
 
     print(f'#{test_case} {ans}')
