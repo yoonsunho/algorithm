9f44468db6f65e34b16b7bf532f4b1e6769e5723
yoonsunho <y_sunho@naver.com>
2025-02-20 18:38:48 +0900

    day11

--- day9/stack2_3x.py
+++ day9/stack2_3x.py
@@ -8,14 +8,25 @@ isp = {'(': 0, '*': 2, '/': 2, '+': 1, '-': 1}   # 스택내부에서의 우선�
 fx = '(6+5*(2-8)/2)'
 susik = ''
 for x in fx:
-    if x not in '+-*/':
+    if x not in '(+-*/)':
         susik += x
     elif x == ")":
         while stack[top] != '(':    # '(' 까지  pop()
             susik += stack[top]     # peek
             top -= 1
         top -= 1    # '(' 버림.pop
-    else
+    else:   # '(+-*/'
+        if top == -1 or isp[stack[top]] < icp[x]:    # 토큰의 우선순위가 더 놏으면
+            top += 1    # push
+            stack[top] = x
+        elif isp[stack[top]] >= icp[x]:     # 더 높은 연산자가 들어있으면 더 낮은 연산자를 만날 떄 까지 pop
+            while top > -1 and isp[stack[top]] >= icp[x]:
+                susik += stack[top]
+                top -= 1
+            top += 1    # 본인 push
+            stack[top] = x
+print(susik)    # 6528-*2/+
+
 
 # susik = '6528-*2/+'
 
