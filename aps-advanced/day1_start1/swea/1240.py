c7528ad140ba10eb99404104d955588022995235
yoonsunho <y_sunho@naver.com>
2025-03-06 17:58:20 +0900

    1240

---
+++ aps-advanced/day1_start1/swea/1240.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("txt/1240.txt", "r")
+
+code_dict = {'0001101': 0, '0011001': 1, '0010011':2, '0111101':3,'0100011':4,'0110001':5,'0101111':6,'0111011':7,'0110111':8,'0001011':9}
+
+T = int(input())
+for tc in range(1, T+1):
+
+    N, M = map(int, input().split())
+
+    matrix = [input() for _ in range(N)]
+    print(matrix)
+
+    for x in
+
+
+
+
+    print(f'#{tc}')
