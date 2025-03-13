5b28aa0bcf10195b4e046a8f63a3b6f7cebc5ff4
yoonsunho <y_sunho@naver.com>
2025-03-13 17:59:51 +0900

    1

---
+++ aps-advanced/day6/swea/5202.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open("txt/5202.txt", "r")
+
+def recur(cnt,start):
+
+    global N
+
+    if start == N:
+        return cnt
+
+    for i in range(start+1, N):
+        if time[start][1] <= time[i][0]:
+            recur(cnt+1, i)
+
+
+T = int(input())
+for tc in range(1, T+1):
+
+    N = int(input())
+
+    time = [[]*N for _ in range(N)]
+    # print(time)
+
+    for i in range(N):
+        st, end = map(int, input().split())
+        time[i] = [st, end]
+
+    time.sort(key=lambda x: x[1])
+    print(time)
+
+
+    result = recur(1,0)
+
+    print(f'#{tc} {result}')
