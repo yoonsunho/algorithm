29ab176a008efe40d35bf13efb5f37d7650b70e8
yoonsunho <y_sunho@naver.com>
2025-03-16 18:59:03 +0900

    1240

--- aps-advanced/day1_start1/swea/1240.py
+++ aps-advanced/day1_start1/swea/1240.py
@@ -1,19 +1,39 @@
 import sys
 sys.stdin = open("txt/1240.txt", "r")
 
-code_dict = {'0001101': 0, '0011001': 1, '0010011':2, '0111101':3,'0100011':4,'0110001':5,'0101111':6,'0111011':7,'0110111':8,'0001011':9}
+code_dict = {'1011000': 0, '1001100': 1, '1100100': 2, '1011110': 3, '1100010': 4, '1000110': 5, '1111010': 6, '1101110': 7, '1110110': 8,'1101000': 9}
 
 T = int(input())
 for tc in range(1, T+1):
 
-    N, M = map(int, input().split())
+    N, M = map(int, input().split())    # 세로, 가로(1<=N <= 50, 56 <= M <= 100)
 
     matrix = [input() for _ in range(N)]
-    print(matrix)
+    # print(matrix)
+    st_i, st_j = -1, -1
 
-    # for x in
+    code_list = []
+    for i in range(N):
+        for j in range(M-1, -1, -1):
+            # print(matrix[i][j], end='')
+            if matrix[i][j] == '1':
+                st_i, st_j = i, j
+                break
+        # print(st_idx)
 
+    for j in range(st_j, st_j-56, -7):
+        code_list.append(matrix[st_i][j:j-7:-1])
 
+    # print(code_list)
 
+    sum_v = 0
+    for i in range(8):
+        code_list[i] = code_dict[code_list[i]]
+        sum_v += code_list[i]
+    # print(code_list)
+    # print((code_list[0]+code_list[2]+code_list[4]+code_list[6])*3 + (code_list[1]+code_list[3]+code_list[5]+code_list[7]))
 
-    print(f'#{tc}')
+    if ((code_list[1]+code_list[3]+code_list[5]+code_list[7])*3 + (code_list[0]+code_list[2]+code_list[4]+code_list[6])) % 10 != 0:
+        sum_v = 0
+
+    print(f'#{tc} {sum_v}')
--- aps-advanced/day5/swea/1861.py
+++ aps-advanced/day5/swea/1861.py
@@ -1,68 +1,68 @@
-import sys
-sys.stdin = open("txt/1861.txt", "r")
-
-di = [0, 1, 0, -1]
-dj = [1, 0, -1, 0]
-
-def dfs(i, j, cnt):
-    global max_cnt
-<<<<<<< HEAD
-
-    max_cnt = max(max_cnt, cnt)
-=======
-    # max_cnt = max(max_cnt, cnt)
->>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
-    # visited[(i, j)] = 1
-
-    for x in range(4):
-        ni, nj = i + di[x], j + dj[x]
-        if 0 <= ni < N and 0 <= nj < N and visited[(ni, nj)] != 1:
-            if matrix[ni][nj]-matrix[i][j] == 1:
-<<<<<<< HEAD
-                visited[(ni, nj)] = 1
-                dfs(ni, nj, cnt+1)
-                visited[(ni, nj)] = 0
-            # if matrix[ni][nj]-matrix[i][j] != 1:
-            #     max_cnt = max(max_cnt, cnt)
-=======
-                # visited[(ni, nj)] = 1
-                dfs(ni, nj, cnt+1)
-                # visited[(ni, nj)] = 0
-    else:
-        max_cnt = max(max_cnt,cnt)
->>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
-
-
-T = int(input())
-for tc in range(1, T+1):
-
-    N = int(input())
-
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    # print(matrix)
-<<<<<<< HEAD
-    visited = {}
-    for i in range(N):
-        for j in range(N):
-            visited[(i, j)] = 0
-    # print(visited)
-=======
-    # visited = {}
-    # for i in range(N):
-    #     for j in range(N):
-    #         visited[(i, j)] = 0
-    # # print(visited)
->>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
-    max_cnt = 0     # 최대 이동 경로
-
-    for i in range(N):
-        for j in range(N):
-<<<<<<< HEAD
-            visited[(i, j)] = 1  # 시작 위치 방문 표시
-            dfs(i, j, 1)
-            visited[(i, j)] = 0  # 시작 위치 초기화
-=======
-            dfs(i, j, 1)
->>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
-    # print(max_cnt)
-    print(f'#{tc} {max_cnt}')
+# import sys
+# sys.stdin = open("txt/1861.txt", "r")
+#
+# di = [0, 1, 0, -1]
+# dj = [1, 0, -1, 0]
+#
+# def dfs(i, j, cnt):
+#     global max_cnt
+# <<<<<<< HEAD
+#
+#     max_cnt = max(max_cnt, cnt)
+# =======
+#     # max_cnt = max(max_cnt, cnt)
+# >>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
+#     # visited[(i, j)] = 1
+#
+#     for x in range(4):
+#         ni, nj = i + di[x], j + dj[x]
+#         if 0 <= ni < N and 0 <= nj < N and visited[(ni, nj)] != 1:
+#             if matrix[ni][nj]-matrix[i][j] == 1:
+# <<<<<<< HEAD
+#                 visited[(ni, nj)] = 1
+#                 dfs(ni, nj, cnt+1)
+#                 visited[(ni, nj)] = 0
+#             # if matrix[ni][nj]-matrix[i][j] != 1:
+#             #     max_cnt = max(max_cnt, cnt)
+# =======
+#                 # visited[(ni, nj)] = 1
+#                 dfs(ni, nj, cnt+1)
+#                 # visited[(ni, nj)] = 0
+#     else:
+#         max_cnt = max(max_cnt,cnt)
+# >>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
+#
+#
+# T = int(input())
+# for tc in range(1, T+1):
+#
+#     N = int(input())
+#
+#     matrix = [list(map(int, input().split())) for _ in range(N)]
+#     # print(matrix)
+# <<<<<<< HEAD
+#     visited = {}
+#     for i in range(N):
+#         for j in range(N):
+#             visited[(i, j)] = 0
+#     # print(visited)
+# =======
+#     # visited = {}
+#     # for i in range(N):
+#     #     for j in range(N):
+#     #         visited[(i, j)] = 0
+#     # # print(visited)
+# >>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
+#     max_cnt = 0     # 최대 이동 경로
+#
+#     for i in range(N):
+#         for j in range(N):
+# <<<<<<< HEAD
+#             visited[(i, j)] = 1  # 시작 위치 방문 표시
+#             dfs(i, j, 1)
+#             visited[(i, j)] = 0  # 시작 위치 초기화
+# =======
+#             dfs(i, j, 1)
+# >>>>>>> 8787830158f866fb3acc2bd67c74febee763eaba
+#     # print(max_cnt)
+#     print(f'#{tc} {max_cnt}')
