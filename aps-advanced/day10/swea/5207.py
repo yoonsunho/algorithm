493c7cacc132dc5c2c0cd910461ef00cf6a4a1c1
yoonsunho <y_sunho@naver.com>
2025-03-18 10:15:50 +0900

    5205

--- aps-advanced/day10/swea/5207.py
+++ aps-advanced/day10/swea/5207.py
@@ -1,17 +1,30 @@
 import sys
 sys.stdin = open("txt/5207.txt", "r")
 
-def bim_search(n):
+def bin_search(target):
+    global cnt
     left = 0
-    right = len(arr)
+    right = len(arr) - 1
+    l_cnt, r_cnt = 0, 0
 
-    while left < right:
+    while left <= right:
 
         mid = (left+right)//2
 
-        if target == arr[mid]:
+        if target == arr[mid]:  # 찾았으면
             cnt += 1
-        left_list = arr
+            break
+
+        if target < arr[mid] and l_cnt == 0:
+            right = mid - 1
+            l_cnt, r_cnt = 1, 0
+
+        elif target > arr[mid] and r_cnt == 0:
+            left = mid + 1
+            l_cnt, r_cnt = 0, 1
+
+        else:
+            break
 
 
 T = int(input())
@@ -19,14 +32,13 @@ for tc in range(1, T+1):
 
     N, M = map(int, input().split())
 
-    arr = list(map(int, input().split()))
+    arr = sorted(list(map(int, input().split())))
     find_list = list(map(int, input().split()))
 
+    cnt = 0
     for x in find_list:
         bin_search(x)
+    # print(cnt)
 
 
-
-
-
-    print(f'#{tc}')
+    print(f'#{tc} {cnt}')
