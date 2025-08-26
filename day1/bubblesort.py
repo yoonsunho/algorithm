410bd700435b901d565fa4a5d9597ba0d86b8bff
yoonsunho <y_sunho@naver.com>
2025-08-26 17:40:51 +0900

    edit bubble

--- day1/bubblesort.py
+++ day1/bubblesort.py
@@ -7,7 +7,7 @@ N = int(input())
 arr = list(map(int, input().split()))
 
 for i in range(N-1,0,-1):       # i = 4,3,2,1
-    for j in range(i):          # j = 0,1,2,3
+    for j in range(i):          # j = 0,1,2,3 / 0,1,2 / 0,1 / 0
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
 print(arr) # [7, 12, 42, 55, 78]
